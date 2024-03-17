783f08d112055c6d3a73d762f1840c31fe1a83dc
777shuang <shuang777@outlook.jp>
2024-03-17 19:31:34 +0900

    Create obsidian.lua

---
+++ obsidian.lua
@@ -0,0 +1,41 @@
+if FORMAT:match 'latex' then
+    function Span(el)
+        for key, value in pairs(el.attributes) do
+            local r, g, b = value:match(".*background *: *#(..)(..)(..).*")
+            if key == "style" and r and g and b then
+                table.insert(
+                    el.content,
+                    1,
+                    pandoc.RawInline(
+                        "latex",
+                        string.format("\\colorbox[rgb]{%f, %f, %f}{", tonumber(r, 16) / 255, tonumber(g, 16) / 255, tonumber(b, 16) / 255)
+                    )
+                )
+                table.insert(el.content, pandoc.RawInline("latex", "}"))
+                return el
+            end
+        end
+
+        return el
+    end
+
+    function RawInline(el)
+        if el.format == "html" then
+            if el.text == "<u>" then
+                return pandoc.RawInline("latex", "\\underline{")
+            elseif el.text == "</u>" then
+                return pandoc.RawInline("latex", "}")
+            end
+            
+            local r, g, b = el.text:match("<font .*color *=.*#(..)(..)(..).*>")
+            if r and g and b then
+                return pandoc.RawInline(
+                    "latex",
+                    string.format("\\textcolor[rgb]{%f, %f, %f}{", tonumber(r, 16) / 255, tonumber(g, 16) / 255, tonumber(b, 16) / 255)
+                )
+            elseif el.text == "</font>" then
+                return pandoc.RawInline("latex", "}")
+            end
+        end
+    end
+end
